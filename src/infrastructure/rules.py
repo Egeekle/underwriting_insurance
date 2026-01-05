9643e47c89a51045061cabe0ef299e7c47416d29
Egeekle <edisonvelazco141@gmail.com>
2026-01-05 15:46:19 -0500

    define historical rules to prediction leads or common clients request

---
+++ src/infrastructure/rules.py
@@ -0,0 +1,23 @@
+import os 
+import joblib
+import pandas as pd
+import numpy as np
+import json
+import random
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics import classification_report, confusion_matrix,ConfusionMatrixDisplay
+from sklearn.utils.class_weight import compute_class_weight
+
+
+use_sample = True
+
+def crear_dataset_rules(n_pos_clas=1000,seed=42):
+    random.seed(seed)
+    np.random.seed(seed)
+
+ 
+    
